14a294cb9d0c616946168d6ab17a30458f879577
SirZeeM <509483@student.saxion.nl>
2022-12-01 14:54:08 +0100

    Added insert functions and values into the models

--- JulianReceiver/messageFormatter.py
+++ JulianReceiver/messageFormatter.py
@@ -46,3 +46,31 @@ def formatMessage(message):
         list.append("Temperature Sensor")
     print(list)
 
+
+def returnMessage(message):
+    list = []
+
+    list.append(find_between(message, "\"device_id\":\"", "\",\"application_ids\""))
+    if message.__contains__(bytes("packetbroker", 'utf-8')):
+        list.append(find_between(message, "\"gateway_id\":\"", "\"},"))
+    else:
+        list.append(find_between(message, "\"gateway_id\":\"", "\",\"eui\":"))
+
+    list.append(find_between(message, "\"time\":\"", "T"))
+    time = find_between(message, "\"time\":\"", "\",\"")
+    list.append(find_between(time, "T", "Z"))
+    if message.__contains__(bytes("BatV", 'utf-8')):
+        list.append(find_between(message, "\"ILL_lmessage\":", ","))
+        list.append("NaN")
+        list.append(find_between(message, "\"Hum_SHT\":", ","))
+        list.append(find_between(message, "\"TempC_SHT\":", ","))
+        list.append(find_between(message, "\"BatV\":", ","))
+        list.append(find_between(message, "Work_mode\":\"", "\""))
+    else:
+        list.append(find_between(message, "\"light\":", ","))
+        list.append(find_between(message, "\"pressure\":", ","))
+        list.append("NaN")
+        list.append(find_between(message, "\"temperature\":", "}"))
+        list.append("inf")
+        list.append("Temperature Sensor")
+    return list
--- my_server/webserver/views.py
+++ my_server/webserver/views.py
@@ -1,5 +1,75 @@
+
 from django.shortcuts import render
 from django.http import HttpResponse
+
+from .models import DeviceType
+from .models import Device
+from .models import PyEntries
+from .models import LhtEntries
+
+import paho.mqtt.client as mqtt
+import time
+
+import messageFormatter
+
+
+
+
+def my_view(request, *args, **kwargs):
+    
+    # Iterate through all the data items
+    
+    DeviceType.objects.create("py", "pycom","battery powered, time, temperature, light, pressure")
+    DeviceType.objects.create("lht", "lhtcom","plugIn, time, temperature, light, Humidity, workmode, batteryVoltage")
+    
+    Device.objects.create("py-saxion", "py", "M.H. Tromplaan 28, 7513 AB, Enschede, Netherlands")
+    Device.objects.create("py-wierden", "py", "Burgemeester J.C. van Den Bergplein 17-18, 7642 GS, Wierden")
+    Device.objects.create("lht-gronau", "lht", "48599 Gronau, North Rhine-Westphalia, Germany")
+    Device.objects.create("lht-wierden", "lht", "Burgemeester J.C. van Den Bergplein 17-18, 7642 GS, Wierden")
+    Device.objects.create("lht-saxion", "lht", "M.H. Tromplaan 28, 7513 AB, Enschede, Netherlands")
+
+    client()
+
+def client():
+    username = "project-software-engineering@ttn"
+    password = "NNSXS.DTT4HTNBXEQDZ4QYU6SG73Q2OXCERCZ6574RVXI.CQE6IG6FYNJOO2MOFMXZVWZE4GXTCC2YXNQNFDLQL4APZMWU6ZGA"
+
+    # The callback for when the client receives a CONNACK response from the server.
+    def on_connect(client, userdata, flags, rc):
+        print("Connected with result code " + str(rc))
+
+        # Subscribing in on_connect() means that if we lose the connection and
+        # reconnect then subscriptions will be renewed.
+        client.subscribe("extapi/data/esm")
+
+    # The callback for when a PUBLISH message is received from the server.
+    def on_message(client, userdata, msg):
+        messageFormatter.formatMessage
+        global values = messageFormatter.returnMessage(msg.payload)
+        if "py" in values[0]:
+            PyEntries.objects.create(values[0],values[1],values[2],values[3],values[4])
+        else:
+            LhtEntries.objects.create(values[0],values[1],values[2],values[3],values[4],values[5],values[6],values[7],values[8])
+        
+    def on_log(client, userdata, level, buf):
+        print("log: ", buf)
+
+
+    client = mqtt.Client()
+    client.on_connect = on_connect
+    client.on_message = on_message
+    # client.on_log = on_log
+    client.username_pw_set(username, password)
+
+    client.connect("eu1.cloud.thethings.network", 1883, 60)
+
+    client.loop_start()
+    client.subscribe("#") 
+    time.sleep(1000)
+
+    client.loop_stop()
+    
+
 # Create your views here.
 def index(request):
     return HttpResponse("Hello World")
